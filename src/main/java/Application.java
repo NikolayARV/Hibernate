79cdb9f2e1574b919dcfa9135cf2308b82b13621
razenkov <razenkov.rna@mail.ru>
2023-04-05 21:08:18 +0300

    Commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,29 @@
+import model.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myPersistenceUnit");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        String jpqlQuery = "SELECT e FROM Employee e WHERE e.age> :minAge";
+        TypedQuery<Employee> query = entityManager.createQuery(jpqlQuery, Employee.class);
+        query.setParameter("minAge", 30);
+        List<Employee> employees = query.getResultList();
+        System.out.println(employees);
+
+        entityManager.getTransaction().commit();
+
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+    }
+}
---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,29 @@
+package model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode (exclude = {"id"})
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name", length = 50, nullable = false)
+    private String first_name;
+    @Column(name = "last_name", length = 50, nullable = false)
+    private String last_name;
+    @Column(name = "gender", length = 50, nullable = false)
+    private String gender;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "city_id")
+    private int city;
+}
